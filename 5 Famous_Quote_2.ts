2bb97c67d93256aca8aee72f5662ec9c78fc87b4
ARISHA-MUGHAL <arishamughal2022@gmail.com>
2024-02-22 01:41:07 +0500

    file Added

---
+++ 5 Famous_Quote_2.ts
@@ -0,0 +1,5 @@
+let famous_person: string = ' Steve Jobs';
+let message :string= '"If you really look closely, most overnight successes took a long time"';
+console.log(`${famous_person} once said, ${message}`)
+
+
